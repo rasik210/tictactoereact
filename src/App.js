3d37ccf1362ed0766da1c4b55b619b5682831a41
Rasik Bihari <rasik210@gmail.com>
2025-05-28 12:52:15 +0530

    centre aligned the board

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function Square({value, onSquareClick }) {
   if(winner){
     status = "Winner: " + winner;
   } else {
-    status = "Next player: " + (xIsNext ? "X" : "O");
+    status = "Next Player: " + (xIsNext ? "X" : "O");
   } 
 
   function calculateWinner(squares){
@@ -78,25 +78,25 @@ function Square({value, onSquareClick }) {
   }
 
   return (
-  <>
-  <div className="status">{status}</div>
-  <div>
-    <div className="board-row">
-      <Square value={squares[0]} onSquareClick ={() => handleClick(0)}/>
-      <Square value={squares[1]} onSquareClick ={() => handleClick(1)}/>
-      <Square value={squares[2]} onSquareClick ={() => handleClick(2)}/>
+  <div className="board">
+    <div className="status">{status}</div>
+    <div>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick ={() => handleClick(0)}/>
+        <Square value={squares[1]} onSquareClick ={() => handleClick(1)}/>
+        <Square value={squares[2]} onSquareClick ={() => handleClick(2)}/>
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick ={() => handleClick(3)}/>
+        <Square value={squares[4]} onSquareClick ={() => handleClick(4)}/> 
+        <Square value={squares[5]} onSquareClick ={() => handleClick(5)}/>
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick ={() => handleClick(6)}/>
+        <Square value={squares[7]} onSquareClick ={() => handleClick(7)}/> 
+        <Square value={squares[8]} onSquareClick ={() => handleClick(8)}/>
+      </div>
     </div>
-    <div className="board-row">
-      <Square value={squares[3]} onSquareClick ={() => handleClick(3)}/>
-      <Square value={squares[4]} onSquareClick ={() => handleClick(4)}/> 
-      <Square value={squares[5]} onSquareClick ={() => handleClick(5)}/>
-    </div>
-    <div className="board-row">
-      <Square value={squares[6]} onSquareClick ={() => handleClick(6)}/>
-      <Square value={squares[7]} onSquareClick ={() => handleClick(7)}/> 
-      <Square value={squares[8]} onSquareClick ={() => handleClick(8)}/>
-    </div>
-    </div>
-  </>
+  </div>
   )
 }
