b1133cdccbf7ab676a422f17f4cb0cb0b404529e
Rasik Bihari <rasik210@gmail.com>
2025-06-03 09:34:15 +0530

    improvised calculate winner function

--- src/App.js
+++ src/App.js
@@ -34,46 +34,22 @@ function Square({value, onSquareClick }) {
   } 
 
   function calculateWinner(squares){
-    //check row 0
-    if (squares[0] == squares[1] && squares[1] == squares[2] && squares[0] != null) {
-      return squares[0];
+    const winnerLines = [
+      [0,1,2], //row 0
+      [3,4,5], //row 1
+      [6,7,8], //row 2
+      [0,3,6], //column 0
+      [1,4,7], //column 1
+      [2,5,8], //column 2
+      [0,4,8], //diagonal 0
+      [2,4,6]  //diagonal 1
+    ];
+    for (let i = 0; i < winnerLines.length; i++) {
+      const [a, b, c] = winnerLines[i]
+      if (squares[a] && squares[a] == squares[b] && squares[b] == squares[c]) {
+        return squares[a]
+      }
     }
-
-    //check row 1
-    if (squares[3] == squares[4] &&  squares[4] == squares[5] && squares[3] != null) {
-      return squares[3] ;
-    }
-
-    //check row 2
-    if (squares[6] == squares[7] && squares[7] == squares[8] && squares[6] != null) {
-      return squares[6] ;
-    }
-
-    //check column 0
-    if (squares[0] == squares[3] && squares[3] == squares[6] && squares[0] != null) {
-      return squares[0];
-    }
-
-    //check column 1
-    if (squares[1] == squares[4] && squares[4] == squares[7] && squares[1] != null) {
-      return squares[1];
-    }
-
-    //check column 2
-    if (squares[2] == squares[5] &&  squares[5] == squares[8] && squares[2] != null) {
-      return squares[2];
-    }
-
-    //check diagonal 0
-    if (squares[0] == squares[4] &&  squares[4] == squares[8] && squares[0] != null) {
-      return squares[0];
-    }
-
-    //check diagonal 1
-    if (squares[2] == squares[4] &&  squares[4] == squares[6] && squares[2] != null) {
-      return squares[2];
-    }
-
     return null;
   }
 
